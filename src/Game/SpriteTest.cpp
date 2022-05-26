6fc6345d1a67369f5319bd25c16663956dc7d156
TigerNav <tiger.navsethi@gmail.com>
2022-05-26 13:14:32 +0000

    holy shit

---
+++ src/Game/SpriteTest.cpp
@@ -0,0 +1,19 @@
+#include "SpriteTest.h"
+
+void SpriteTest::init_sprite() {
+    texture.loadFromFile("/home/navin/programming stuff/Games/top-down/src/Assets/00-guide.png");
+    sprite.setTexture(texture);
+    
+}
+
+void SpriteTest::update_sprite() {
+
+}
+
+void SpriteTest::draw_sprite() {
+    
+}
+
+void SpriteTest::unload_sprite() {
+
+}
